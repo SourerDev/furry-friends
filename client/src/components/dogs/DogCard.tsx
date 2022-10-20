e6d06e82f9dcec0d8983733448ed5f835ba4ec76
Jhonny_Ale <jhonny30ca@gmail.com>
2022-10-20 16:46:22 -0500

    Adding css to dog card

--- client/src/components/dogs/DogCard.tsx
+++ client/src/components/dogs/DogCard.tsx
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
 import { Dogs } from "../../interfaces/interfaces";
-const styles = require('./Dogcard.module.css').default
+const styles = require("./Dogcard.module.css").default;
 
 const DogCard = (props: Dogs) => {
   return (
     <div className={styles.card}>
-    
       <div className={styles.image}>
         <img src={props.image} alt={props.name} />
       </div>
-      <div className={styles.attributes}>
-        <NavLink to={`/dog/${props.id}`}><h1>{props.name}</h1></NavLink>
 
+      <div className={styles.description}>
+        <div style={{}}>
+          <h3 className={styles.name}>San Carlo</h3>
+        </div>
+        <div style={{ textAlign: "left", display: "flex" ,flexDirection:'column',justifyContent:'center',}}>
+          <p className={styles.weight}>Weight: 12 - 12 cm</p>
+          <p className={styles.temp}>Temperaments: canson, odioso</p>
+        </div>
       </div>
-
     </div>
   );
 };
